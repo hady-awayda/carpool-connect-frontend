6c24ed9739b838b6ed3434bee223fcbf7d4fed96
hady-awayda <hady_awayda@hotmail.com>
2024-09-12 19:20:18 +0300

    Modified animation between pages to simple_push

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <Stack screenOptions={{ headerShown: false }}>
+      <Stack screenOptions={{ headerShown: false, animation: "simple_push" }}>
         <Stack.Screen
           name="index"
           options={{
