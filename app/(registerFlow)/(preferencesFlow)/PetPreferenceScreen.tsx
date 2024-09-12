02e6167dc2da37292913cb035af25dde9accd8b8
hady-awayda <hady_awayda@hotmail.com>
2024-09-12 18:43:13 +0300

    Added finish later button to pets preference page

--- app/(registerFlow)/(preferencesFlow)/PetPreferenceScreen.tsx
+++ app/(registerFlow)/(preferencesFlow)/PetPreferenceScreen.tsx
@@ -29,6 +29,9 @@ export default function PetPreferenceScreen() {
       <TouchableOpacity style={styles.backButton} onPress={() => router.back()}>
         <Text style={styles.backButtonText}>Back</Text>
       </TouchableOpacity>
+      <Text style={styles.FinishLaterText} onPress={() => router.push("/Home")}>
+        Finish Later
+      </Text>
     </View>
   );
 }
@@ -73,5 +76,12 @@ const styles = StyleSheet.create({
     color: "#333",
     fontSize: 16,
     textAlign: "center",
+  },
+  FinishLaterText: {
+    color: "#333",
+    fontSize: 16,
+    textAlign: "center",
+    marginTop: 50,
+    textDecorationLine: "underline",
   },
 });
