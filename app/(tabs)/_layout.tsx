ca3284a7be5b27dfff0ae9468f48ed4645b30196
hady-awayda <hady_awayda@hotmail.com>
2024-09-15 18:20:49 +0300

    Removed the tab bar border and shadow

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -48,6 +48,7 @@ export default function TabLayout() {
           paddingBottom: insets.bottom > 0 ? insets.bottom + 25 : 25,
           paddingTop: insets.bottom > 0 ? insets.bottom + 5 : 5,
           height: insets.bottom > 0 ? insets.bottom + 80 : 65,
+          elevation: -100,
         },
         tabBarActiveTintColor: Colors.light.primary,
         tabBarInactiveTintColor: "#999",
