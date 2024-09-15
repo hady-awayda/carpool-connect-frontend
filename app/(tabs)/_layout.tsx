153395f84063dd1dcc5b8958cb7cb356a289816f
hady-awayda <hady_awayda@hotmail.com>
2024-09-15 18:15:33 +0300

    Adjusted bottom padding after removing titles

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -45,7 +45,7 @@ export default function TabLayout() {
         tabBarShowLabel: false,
         tabBarStyle: {
           display: isTabBarVisible ? "flex" : "none",
-          paddingBottom: insets.bottom > 0 ? insets.bottom + 25 : 10,
+          paddingBottom: insets.bottom > 0 ? insets.bottom + 25 : 25,
           paddingTop: insets.bottom > 0 ? insets.bottom + 5 : 5,
           height: insets.bottom > 0 ? insets.bottom + 80 : 65,
         },
