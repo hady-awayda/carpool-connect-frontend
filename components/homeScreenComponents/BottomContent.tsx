de9492b69e4df9149ff86fde60ad36c4b58213f7
hady-awayda <hady.awayda@gmail.com>
2024-09-18 22:19:04 +0300

    Implemented the functionality to handle setting location to redux slice from map marker

--- components/homeScreenComponents/BottomContent.tsx
+++ components/homeScreenComponents/BottomContent.tsx
@@ -1,11 +1,19 @@
 import { Colors } from "@/constants/Variables";
 import {
+  setDeparture,
+  setDestination,
+} from "@/data/redux/addressListSlice/slice";
+import { RootState } from "@/data/redux/store";
+import {
   Animated,
   Dimensions,
   StyleSheet,
   TouchableOpacity,
 } from "react-native";
+import { useDispatch, useSelector } from "react-redux";
+import BoldButton from "../BoldButton";
 import DestinationField from "./DestinationField";
+import { LocationCoords } from "./interfaces";
 import LastDestinations from "./LastThreeDestinations";
 
 type BottomContentProps = {
@@ -13,10 +21,40 @@ type BottomContentProps = {
 };
 
 const BottomContent: React.FC<BottomContentProps> = ({ showRouteSheet }) => {
+  const uiState = useSelector((state: RootState) => state.uiState.uiState);
+  const dispatch = useDispatch();
+
+  const getAddressName = async (): Promise<string> => {
+    return "";
+  };
+
+  const getMarkerCoordinates = async (): Promise<LocationCoords> => {
+    return { latitude: 0, longitude: 0, latitudeDelta: 0, longitudeDelta: 0 };
+  };
+
+  const handleSettingLocation = async () => {
+    const name = await getAddressName();
+    const coords: LocationCoords = await getMarkerCoordinates();
+
+    if (uiState === "setting-departure") {
+      dispatch(setDeparture({ name: "", coords }));
+    } else if (uiState === "setting-destination") {
+      dispatch(setDestination({ name: "", coords }));
+    }
+  };
+
   return (
     <Animated.View style={[styles.container]}>
       <TouchableOpacity style={styles.slider}></TouchableOpacity>
-      <DestinationField showRouteSheet={showRouteSheet} />
+      {uiState === "setting-departure" || uiState === "setting-destination" ? (
+        <BoldButton
+          buttonText="Confirm Location"
+          buttonStyle={{ backgroundColor: Colors.light.primary }}
+          onPress={handleSettingLocation}
+        />
+      ) : (
+        <DestinationField showRouteSheet={showRouteSheet} />
+      )}
       <LastDestinations />
     </Animated.View>
   );
