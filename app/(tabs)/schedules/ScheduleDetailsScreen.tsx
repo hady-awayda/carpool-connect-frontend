b4a464e44e67926edcd99d15ea74f596e62c27c4
hady-awayda <hady_awayda@hotmail.com>
2024-09-24 09:04:13 +0300

    Adjusted styling for schedule card in schedule details screen

--- app/(tabs)/schedules/ScheduleDetailsScreen.tsx
+++ app/(tabs)/schedules/ScheduleDetailsScreen.tsx
@@ -13,6 +13,8 @@ import {
 import Mapbox from "../../../components/Mapbox";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 
+const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
 const ScheduleDetails: React.FC = () => {
   const router = useRouter();
   const { id, schedule } = useLocalSearchParams();
@@ -29,6 +31,7 @@ const ScheduleDetails: React.FC = () => {
     destinationName = "Unknown",
     departureTime,
     arrivalTime,
+    schedulePattern = [],
   } = scheduleData;
 
   const formatTime = (time: Date) => {
@@ -42,6 +45,13 @@ const ScheduleDetails: React.FC = () => {
     hours = hours ? hours : 12;
 
     return `${hours}:${minutes} ${ampm}`;
+  };
+
+  const handleProfileNavigation = (userId: number) => {
+    router.push({
+      pathname: "/(profiles)UserProfile",
+      params: { id: userId, schedule: JSON.stringify(schedule) },
+    });
   };
 
   if (!scheduleData) {
@@ -62,22 +72,60 @@ const ScheduleDetails: React.FC = () => {
             color={Colors.light.text}
           />
         </TouchableOpacity>
-        <Text style={styles.title}>Schedule Details</Text>
+        <Text style={styles.headerTitle}>Schedule Details</Text>
       </View>
+
       <View style={styles.scheduleCard}>
         <View>
-          {currentTab !== "schedules" && (
+          {currentTab === "schedules" && (
             <BoldButton
-              buttonText="Go to User Profile"
-              onPress={() => console.log("Navigating to User Profile")}
+              buttonText="View Profile"
+              onPress={() => handleProfileNavigation(userId)}
             />
           )}
         </View>
-        <View>
-          <Text>From: {departureName}</Text>
-          <Text>To: {destinationName}</Text>
-          <Text>Departure Time: {formatTime(departureTime)}</Text>
-          <Text>Arrival Time: {formatTime(arrivalTime)}</Text>
+        <View style={styles.infoContainer}>
+          <Text style={styles.title}>
+            {scheduleType[0].toUpperCase() + scheduleType.slice(1)}
+          </Text>
+          <View style={styles.textContainer}>
+            <Text style={styles.leftText}>From:</Text>
+            <Text style={styles.text}>
+              {departureName?.split(" ").slice(0, 3).join(" ")}
+            </Text>
+          </View>
+
+          <View style={styles.textContainer}>
+            <Text style={styles.leftText}>Departure:</Text>
+            <Text style={styles.text}>{formatTime(departureTime)}</Text>
+          </View>
+          <View style={styles.textContainer}>
+            <Text style={styles.leftText}>To:</Text>
+            <Text style={styles.text}>
+              {destinationName?.split(" ").slice(0, 3).join(" ")}
+            </Text>
+          </View>
+          <View style={styles.textContainer}>
+            <Text style={styles.leftText}>Arrival:</Text>
+            <Text style={styles.text}>{formatTime(arrivalTime)}</Text>
+          </View>
+          <View style={styles.weekdayContainer}>
+            {schedulePattern && schedulePattern.length > 0 ? (
+              daysOfWeek.map((day, index) => (
+                <View
+                  key={index}
+                  style={[
+                    styles.dayCircle,
+                    schedulePattern[index] && styles.highlightedDay,
+                  ]}
+                >
+                  <Text style={styles.dayText}>{day}</Text>
+                </View>
+              ))
+            ) : (
+              <Text style={styles.text}></Text>
+            )}
+          </View>
         </View>
       </View>
 
@@ -107,7 +155,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     width: width * 0.9,
   },
-  title: {
+  headerTitle: {
     ...Typography.heading,
     marginLeft: width / 8,
   },
@@ -119,7 +167,51 @@ const styles = StyleSheet.create({
     alignItems: "center",
     borderWidth: 1,
     borderRadius: 8,
-    width: width * 0.9,
+    width: width * 0.95,
+  },
+  infoContainer: {
+    flex: 1,
+    width: width * 0.8,
+  },
+  title: {
+    ...Typography.subheading,
+    marginBottom: 12,
+  },
+  textContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "flex-end",
+  },
+  leftText: {
+    ...Typography.title,
+    color: Colors.light.text,
+  },
+  text: {
+    ...Typography.text,
+    overflow: "hidden",
+    height: 20,
+  },
+  weekdayContainer: {
+    flexDirection: "row",
+    marginTop: 8,
+  },
+  dayCircle: {
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+    justifyContent: "center",
+    alignItems: "center",
+    borderWidth: 1,
+    borderColor: "#ccc",
+    marginHorizontal: 3,
+  },
+  highlightedDay: {
+    backgroundColor: "#4CAF50",
+  },
+  dayText: {
+    fontSize: 12,
+    fontWeight: "bold",
+    color: "#fff",
   },
 });
 
